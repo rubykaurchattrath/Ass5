4ff2255bbb1d5e99c5854ee6bc8d9b363de5075d
Jasjeen Kaur Khosa <jasjeen@Jasjeens-MacBook-Air.local>
2023-05-28 22:21:28 -0700

    i think im doing this right

--- ThreadedBinarySearchTree.h
+++ ThreadedBinarySearchTree.h
@@ -1,16 +1,18 @@
+
 #ifndef THREADED_BINARY_SEARCH_TREE_H
 #define THREADED_BINARY_SEARCH_TREE_H
 
 template <class KeyType>
+
 class ThreadedBSTNode {
 public:
     KeyType key;
     ThreadedBSTNode* left;
     ThreadedBSTNode* right;
     bool leftThread;
-    bool rightThread;
+    bool rightThread; 
 
-    ThreadedBSTNode(const KeyType& newKey)
+    ThreadedBSTNode(const KeyType& newKey) //constructors 
         : key(newKey), left(nullptr), right(nullptr),
           leftThread(false), rightThread(false) {}
 };
@@ -57,3 +59,34 @@ private:
 };
 
 #endif // THREADED_BINARY_SEARCH_TREE_H
+
+
+/**
+ * 
+ * /** A class of nodes for a link-based binary tree.
+@file BinaryNode.h 
+#ifndef _BINARY_NODE
+#define _BINARY_NODE
+template < class ItemType>
+class BinaryNode
+{
+private :
+ItemType item; // Data portion
+BinaryNode<ItemType>* leftChildPtr; // Pointer to left child
+BinaryNode<ItemType>* rightChildPtr; // Pointer to right child
+
+public :
+BinaryNode();
+BinaryNode( const ItemType& anItem);
+BinaryNode( const ItemType& anItem, BinaryNode<ItemType>* leftPtr, BinaryNode<ItemType>* rightPtr);
+void setItem( const ItemType& anItem);
+ItemType getItem() const ;
+bool isLeaf() const ;
+BinaryNode<ItemType>* getLeftChildPtr() const ;
+BinaryNode<ItemType>* getRightChildPtr() const ;
+void setLeftChildPtr(BinaryNode<ItemType>* leftPtr);
+void setRightChildPtr(BinaryNode<ItemType>* rightPtr);
+}; // end BinaryNode
+#include "BinaryNode.cpp"
+#endif
+*/
