023beacf3973b9d7bd49f2e52baeec691fbd71cd
Ruby Chattrath <122198061+rubykaurchattrath@users.noreply.github.com>
2023-06-02 18:57:31 -0700

    Update ThreadedBST.h

--- ThreadedBST.h
+++ ThreadedBST.h
@@ -12,18 +12,18 @@ private:
 
 public:
     ThreadedBST(); // constructor
-    ThreadedBST(const ThreadedBST& other); // copy constructor
     ~ThreadedBST(); // destructor
 
     void insert(int key);
     void remove(int key);
     void display();
     void inorder();
-    Node* copyTree(Node* node);
 
 private:
     Node* findSuccessor(Node* node);
     Node* findSuccessorParent(Node* node);
+    ThreadedBST(const ThreadedBST& other); // copy constructor
+    Node* copyTree(Node* node); // recursive helper function to copy the tree
 };
 
 #endif  // THREADEDBST_H
